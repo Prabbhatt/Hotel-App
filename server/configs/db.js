211caaf6fe30d48298e5c5060aaedf613fb63d1d
Prabhat SInghal <prabhatsinghal869@gmail.com>
2026-06-10 01:48:31 +0530

    done

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,11 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    console.log("===== CONNECT DB START =====");
-    console.log("MONGODB_URI exists:", !!process.env.MONGODB_URI);
-
-    await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`);
-
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("✅ Database Connected");
   } catch (error) {
     console.error("❌ Mongo Connection Error:", error);
